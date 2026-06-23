ff3b21b01834717d1de67c895f7bdf23abe250c5
bhavik goyal <bhavikgoyal@gmail.com>
2026-06-23 18:39:57 +0530

    solved

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -187,6 +187,7 @@ export default function AppRoutes() {
 <Route path="Members/Edit/:id" element={<MembersEdit />} />
           <Route path="*" element={<RoleRedirect />} />
          <Route path="startstop" element={<StartStopActivity />} />
+          <Route path="task-reports" element={<StartStopActivity />} />
         </Route>
 
         <Route path="*" element={<Navigate to={user ? getDefaultRoute(user.role) : '/login'} replace />} />
